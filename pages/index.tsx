016e59fa1cb552099241c2914e6682815ef32cf5
TypicalEgg <erikastaroza@gmail.com>
2022-10-19 14:00:33 -0700

    Added event handlers to widget. Worked on widget UI.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,44 @@
 import type { NextPage } from 'next'
+import axios from 'axios';
 
 import styles from '../styles/Widget.module.css'
+import { ChangeEvent } from 'react';
+
+const regex:RegExp = /[\(\)\- ]/g;
+var countryCode:number = 1;
+var phoneNumber:string = "";
+
+function onPhoneNumberInputChanged(event:ChangeEvent<HTMLInputElement>)
+{
+    phoneNumber = event.target.value.trim();
+}
+
+function onCallButtonClicked()
+{
+    var parsedNumber:string = phoneNumber.replaceAll(regex, "");
+
+    axios.post(
+        "/api/call",
+        {
+            "uid": "",
+            "phoneNumber": `+${countryCode}${parsedNumber}`
+        }
+    );
+}
 
 const Widget:NextPage = () => {
     return (
         <div className={styles.widget}>
             <div className={styles.center}>
                 <h1 className={styles.title}>Title</h1>
-                <input/>
+                <div className={styles.row}>
+                    <button>Country</button>
+                    <div style={{"width": 5}}></div>
+                    <input id="phoneNumberInput" onChange={onPhoneNumberInputChanged}/>
+                </div>
             </div>
-            <div style={{"position": "relative", "height": "75%"}}></div>
-            <button className={styles.call_btn}>Call Us</button>
+            <div style={{"position": "relative", "height": "50%"}}></div>
+            <button className={styles.call_btn} onClick={onCallButtonClicked}>Call Us</button>
             <button className={styles.close_btn}>X</button>
         </div>
     );
