6ae92246020bcc28c61c4385c6cea477fdcc5eff
TypicalEgg <erikastaroza@gmail.com>
2022-11-21 23:06:53 -0800

    Refactor

--- test/app.js
+++ test/app.js
@@ -2,78 +2,83 @@ const iframeId = "callWidgetIFrame";
 const overlayId = "callWidgetOverlay";
 const iframeUrl = "http://localhost:3000";
 
-var isOpen = true;
-
-/**
- * Initializes the call widget by adding an IFrame element to the body
- * of the document. An `init` message is sent once the IFrame is loaded.
- * 
- * @param {string} title The title of the widget.
- * @param {string} buttonText The text of the call button.
- */
-function initCallWidget(title, buttonText)
+class CallWidget
 {
-    var iframe = document.createElement("iframe");
-    iframe.id = iframeId;
-    iframe.src = iframeUrl;
-    iframe.style.border = "None";
-    iframe.style.width = "300px";
-    iframe.style.height = "300px";
-    iframe.style.position = "fixed";
-    iframe.style.top = "50%";
-    iframe.style.left = "50%";
-    iframe.style.transform = "translate(-50%, -50%)";
+    isOpen = true;
 
-    var overlay = document.createElement("div");
-    overlay.id = overlayId;
-    overlay.style.position = "absolute";
-    overlay.style.top = "0";
-    overlay.style.left = "0";
-    overlay.style.right = "0";
-    overlay.style.bottom = "0";
-    overlay.style.height = "100%";
-    overlay.style.width = "100%";
-    overlay.style.backgroundColor = "rgba(0, 0, 0, .5)";
-    overlay.onclick = () => closeCallWidget();
-
-    overlay.appendChild(iframe);
-
-    document.body.appendChild(overlay);
-
-    function onMessage(e)
+    /**
+     * Initializes the call widget by adding an IFrame element to the body
+     * of the document. An `init` message is sent once the IFrame is loaded.
+     * 
+     * @param {string} title The title of the widget.
+     * @param {string} buttonText The text of the call button.
+     */
+    init(title, buttonText)
     {
-        if(e.origin !== iframeUrl) return;
+        var iframe = document.createElement("iframe");
+        iframe.id = iframeId;
+        iframe.src = iframeUrl;
+        iframe.style.border = "None";
+        iframe.style.width = "300px";
+        iframe.style.height = "300px";
+        iframe.style.position = "fixed";
+        iframe.style.top = "50%";
+        iframe.style.left = "50%";
+        iframe.style.transform = "translate(-50%, -50%)";
 
-        if(e.data == "done")
-        {
-            iframe.contentWindow.postMessage({
-                "message": "init",
-                "contents": {
-                    "title": title,
-                    "buttonText": buttonText
-                }
-            }, "*");
-            return;
-        }
-        else if(e.data["message"] == "openState")
-        {
-            isOpen = e.data["contents"];
+        var overlay = document.createElement("div");
+        overlay.id = overlayId;
+        overlay.style.position = "absolute";
+        overlay.style.top = "0";
+        overlay.style.left = "0";
+        overlay.style.right = "0";
+        overlay.style.bottom = "0";
+        overlay.style.height = "100%";
+        overlay.style.width = "100%";
+        overlay.style.backgroundColor = "rgba(0, 0, 0, .5)";
+        overlay.onclick = () => closeCallWidget();
 
-            if(!isOpen) closeCallWidget();
+        overlay.appendChild(iframe);
+
+        document.body.appendChild(overlay);
+
+        function onMessage(e)
+        {
+            if(e.origin !== iframeUrl) return;
+
+            if(e.data == "done")
+            {
+                iframe.contentWindow.postMessage({
+                    "message": "init",
+                    "contents": {
+                        "title": title,
+                        "buttonText": buttonText
+                    }
+                }, "*");
+                return;
+            }
+            else if(e.data["message"] == "openState")
+            {
+                isOpen = e.data["contents"];
+
+                if(!isOpen) closeCallWidget();
+            }
         }
+
+        window.addEventListener("message", onMessage);
     }
 
-    window.addEventListener("message", onMessage);
+    openCallWidget()
+    {
+        var overlay = document.getElementById(overlayId);
+        overlay.style.display = "block";
+    }
+
+    closeCallWidget()
+    {
+        var overlay = document.getElementById(overlayId);
+        overlay.style.display = "none";
+    }
 }
 
-function openCallWidget()
-{
-    var overlay = document.getElementById(overlayId);
-    overlay.style.display = "block";
-}
-
-function closeCallWidget()
-{
-    var overlay = document.getElementById(overlayId);
-    overlay.style.display = "none";
-}
+onCallWidgetReady(new CallWidget())
